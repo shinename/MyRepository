f95ec0670bd9360164db5bea3c9b42cbfb195668
shine <253866401@qq.com>
2019-05-21 18:14:43 +0800

    add two lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println("Houston Shine !");
+		System.out.println("Livechance0 !");
+		System.out.println("Livechance1 !");
 	}
 	
 }

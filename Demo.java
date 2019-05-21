32ee57b2695513b5a39106141aa3021d5b5dc3a6
shine <253866401@qq.com>
2019-05-21 19:02:08 +0800

    add

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Livechance0 !");
 		System.out.println("Livechance1 !");
-		System.out.println("Livechance2 !!");
+		System.out.println("Livechance2 !");
 	}
 	
 }

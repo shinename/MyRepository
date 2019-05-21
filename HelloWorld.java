39d716e342eaa48229ec281dc4b0bd964d9169c3
shine <253866401@qq.com>
2019-05-21 18:20:05 +0800

    This is a test.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Houston Shine !");
 		System.out.println("Livechance0 !");
 		System.out.println("Livechance1 !");
+		System.out.println("Livechance2 !");
 	}
 	
 }
